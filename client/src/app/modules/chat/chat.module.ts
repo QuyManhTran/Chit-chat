16313a71ea32a6135883968a86756fc9e0998335
quymanh <quymanhtran453@gmail.com>
2024-02-19 23:19:54 +0700

    ADD: header(avatar, search) and sidebar(scrollbar)

--- client/src/app/modules/chat/chat.module.ts
+++ client/src/app/modules/chat/chat.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatComponent } from './chat.component';
-import { HeaderComponent } from './components/header/header/header.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    ChatComponent,
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule,
-    ChatRoutingModule
-  ]
+    declarations: [ChatComponent, HeaderComponent, SidebarComponent],
+    imports: [CommonModule, ChatRoutingModule],
 })
-export class ChatModule { }
+export class ChatModule {}
